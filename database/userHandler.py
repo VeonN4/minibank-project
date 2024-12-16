c138849f1b180d40562c7f637baa3ff933b5ca26
VeonN4 <fxzia13@gmail.com>
2024-12-16 09:29:29 +0700

    fix(userHandler): Fix if the user_data is none it return 0

--- database/userHandler.py
+++ database/userHandler.py
@@ -1,16 +1,21 @@
 from . import DatabaseManager
 
-def validateUser(user: str, pin: int):
-    user_data = DatabaseManager.collection.find_one({'name': user})
 
-    if user == user_data["name"] and pin == user_data["pin"]:
+def validateUser(user: str, pin: int):
+    user_data = DatabaseManager.collection.find_one({"name": user})
+
+    if user_data["name"] is None:
+        return 0
+    elif user == user_data["name"] and pin == user_data["pin"]:
         return 1
     else:
         return 0
 
+
 def create_user(user: str, pin: int):
-    user_data = {'name': user, 'pin': pin, 'money': 10000}
+    user_data = {"name": user, "pin": pin, "money": 10000}
     DatabaseManager.collection.insert_one(user_data)
 
+
 def get_user(user: str):
-    return DatabaseManager.collection.find_one({'name': user})
+    return DatabaseManager.collection.find_one({"name": user})
